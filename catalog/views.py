7f0c234411b2409a81f8df449165aacaf2af865c
RizwanNiazFolio3 <rizwanniaz@folio3.com>
2021-06-28 14:54:29 +0500

    Added a counter for number of hompege visits using sessions in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -23,6 +23,9 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -30,6 +33,7 @@ def index(request):
         'num_authors': num_authors,
         'num_fantasy_genres' : num_fantasy_genres,
         'num_lotr_books' : num_lotr_books,
+        'num_visits' : num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
