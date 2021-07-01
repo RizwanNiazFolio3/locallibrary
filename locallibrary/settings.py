f61fb54200630a850d19acd3c2b468f3703fee52
RizwanNiazFolio3 <rizwanniaz@folio3.com>
2021-07-01 17:53:12 +0500

    Fixed typo in allowed host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['tim-moose-37514.herokuapp.com.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['tim-moose-37514.herokuapp.com','127.0.0.1']
 
 
 # Application definition
