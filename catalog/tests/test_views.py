f9b2420808adf3871fc158b123361f78881c4df7
RizwanNiazFolio3 <rizwanniaz@folio3.com>
2021-07-08 17:56:03 +0500

    Added tests for author detail view

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -508,3 +508,28 @@ class BookListViewTest(TestCase):
         self.assertEqual(len(response.context['book_list']), 3)
 
 
+class AuthorDetailViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 13 authors for pagination tests
+        Author.objects.create(
+            first_name='Christian',
+            last_name='Surname',
+        )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalog/author/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('author-detail',kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('author-detail',kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/author_detail.html')
+
+    def test_http404_for_invalid_book(self):
+        response = self.client.get(reverse('author-detail',kwargs={'pk': 2}))
+        self.assertEqual(response.status_code, 404)
