f119dfdc6a0c48ba6d318ff70d83eca28ae42d80
Hammad Hassan <hammadhassan@folio3.com>
2021-08-03 11:09:50 +0500

    Added librarian permission to book crud api

--- catalog/book_api.py
+++ catalog/book_api.py
@@ -1,12 +1,15 @@
 from rest_framework import viewsets, permissions
 from .serializers import BookSerializer
 from .models import Book
+from .permissions import IsLibrarian
 
 
 class BookViewSet(viewsets.ModelViewSet):
     """This viewset provides create, retrieve, update and delete apis for books"""
 
-
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
+    permission_classes = [
+        IsLibrarian
+    ]
