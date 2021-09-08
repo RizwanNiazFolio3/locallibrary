4a093ba2a170d5093cc2a5f88de95e6a886b9249
RizwanNiazFolio3 <rizwanniaz@folio3.com>
2021-09-08 14:57:07 +0500

    Added type hinting to Custom token serializer

--- catalog/custom_tokens.py
+++ catalog/custom_tokens.py
@@ -1,5 +1,7 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import Token
+from django.contrib.auth.models import User
 
 class AddIsLibrarianClaimSerializer(TokenObtainPairSerializer):
     '''
@@ -7,7 +9,7 @@ class AddIsLibrarianClaimSerializer(TokenObtainPairSerializer):
     can be added to the payload of a token, allowing us to encode user permissions into the token itself
     '''
     @classmethod
-    def get_token(cls, user):
+    def get_token(cls, user: User) -> Token:
         token = super().get_token(user)
 
         # Add custom claims
